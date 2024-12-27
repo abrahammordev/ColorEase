ce848f0210a3beb3ca12fec15974af8068706ec7
LozzDev <lozzdeveloper@gmail.com>
2024-12-27 13:56:00 +0100

    Increase iteration count for optimal color combination generation

--- src/optimalColor.js
+++ src/optimalColor.js
@@ -2,7 +2,7 @@ import { contrastRatio } from './colorChecker.js';
 
 function generateOptimalcombination(){
     let optimalUmbral = 50;
-    for(let i = 0 ; i < 1000; i++){
+    for(let i = 0 ; i < 10000; i++){
         let firstColor = generateRandomColor();
         let secondColor = generateRandomColor();
         let contrast = contrastRatio(firstColor, secondColor);
