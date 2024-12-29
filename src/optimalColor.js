8348902e80cde7caccc866069b8b8d9c3274addb
abrahammordev <abrahammordev@gmail.com>
2024-12-29 19:42:27 +0100

    Refactor optimalColor.js to improve color generation logic and enhance contrast calculation

--- src/optimalColor.js
+++ src/optimalColor.js
@@ -1,20 +1,30 @@
-import { contrastRatio } from './colorChecker.js';
+import { contrastRatio } from "./colorChecker.js";
 
-function generateOptimalcombination(){
-    let optimalUmbral = 50;
-    for(let i = 0 ; i < 10000; i++){
-        let firstColor = generateRandomColor();
-        let secondColor = generateRandomColor();
-        let contrast = contrastRatio(firstColor, secondColor);
-        if(contrast >= optimalUmbral){
-            return [firstColor, secondColor];
-        }
+function generateOptimalcombination() {
+  let optimalUmbral = 50;
+  let bestFirstColor = generateRandomColor();
+  let bestSecondColor = generateRandomColor();
+  let bestContrast = contrastRatio(bestFirstColor, bestSecondColor);
+  for (let i = 0; i < 10000; i++) {
+    let firstColor = generateRandomColor();
+    let secondColor = generateRandomColor();
+    let contrast = contrastRatio(firstColor, secondColor);
+    if (contrast >= optimalUmbral) {
+      return [firstColor, secondColor];
     }
-    
+    if (contrast > bestContrast) {
+      bestFirstColor = firstColor;
+      bestSecondColor = secondColor;
+      bestContrast = contrast;
+    }
+  }
+
+  return [bestFirstColor, bestSecondColor];
 }
 
-function generateRandomColor(){
-    return "#" + Math.floor(Math.random()*16777215).toString(16);
+function generateRandomColor() {
+  let color = Math.floor(Math.random() * 16777215).toString(16);
+  return "#" + color.padStart(6, "0");
 }
 
 export { generateOptimalcombination };
