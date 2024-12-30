98c466e8db94b459c775406d767353b07fead2e2
abrahammordev <abrahammordev@gmail.com>
2024-12-30 21:15:52 +0100

    Add ColorBlindIcon component in order to improve accesibility

--- src/components/LeftBar.jsx
+++ src/components/LeftBar.jsx
@@ -4,21 +4,21 @@ import LogoIcon from "./icons/LogoIcon";
 import ProfileIcon from "./icons/ProfileIcon";
 import SettingsIcon from "./icons/SettingsIcon";
 import styles from "./LeftBar.module.css";
+import ColorBlindIcon from "./icons/ColorBlindIcon";
 
-function LeftBar(){
-    return(
-        <div className={styles.LeftBar}>
-            <div className={styles.LogoIcon}>
-                <LogoIcon/>
-            </div>
-            
-            <div className={styles.Icons}>
-                <SettingsIcon/> 
-                <DarkmodeIcon/>
-            </div>
-                
-        </div>
-    )
+function LeftBar() {
+  return (
+    <div className={styles.LeftBar}>
+      <div className={styles.LogoIcon}>
+        <LogoIcon />
+      </div>
+
+      <div className={styles.Icons}>
+        <SettingsIcon />
+        <DarkmodeIcon />
+      </div>
+    </div>
+  );
 }
 
 export default LeftBar;
