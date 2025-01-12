232e79cea8bd7e40f7d6d2e6539fb2cd4a7e43fb
abrahammordev <abrahammordev@gmail.com>
2025-01-12 19:21:56 +0100

    Bump version to 1.0.0, add @vercel/analytics dependency, and integrate analytics component in App

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import MainView from "./components/MainView";
 import Preview from "./components/Preview";
 import styles from "./App.module.css";
 import { ThemeProvider } from "./context/ThemeContext";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
     const [firstColor, setFirstColor] = useState("#ffffff");
@@ -15,13 +16,14 @@ function App() {
     };
 
     return (
-        <ThemeProvider>
-            <div className={styles.appContainer}>
-                <LeftBar className={styles.leftBar} />
-                <MainView onUpdateColors={handleUpdateColors} />
-                <Preview firstColor={firstColor} secondColor={secondColor} />
-            </div>
-        </ThemeProvider>
+      <ThemeProvider>
+        <div className={styles.appContainer}>
+          <Analytics />
+          <LeftBar className={styles.leftBar} />
+          <MainView onUpdateColors={handleUpdateColors} />
+          <Preview firstColor={firstColor} secondColor={secondColor} />
+        </div>
+      </ThemeProvider>
     );
 }
 
