49a67f9aa63e70519a877490bd0b4a022e79d5e8
abrahammordev <abrahammordev@gmail.com>
2025-01-12 19:10:41 +0100

    Update .gitignore and clean up imports in components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import LeftBar from "./components/LeftBar";
 import MainView from "./components/MainView";
 import Preview from "./components/Preview";
